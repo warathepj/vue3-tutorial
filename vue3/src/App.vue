5a59480c3f2c5ea7a860f0448b927a7d223a9e0c
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-04 10:12:23 +0700

    1 27 51

---
+++ vue3/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Dc Heros</h1>
+  <ul>
+    <li v-for="(hero, index) in dcHeros" v-bind:key="index">{{ index }} : {{ hero.name }}</li>
+
+
+    
+  </ul>
+  <input v-bind:value='newHero' type="">
+  <button>Add Hero</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+      newHero: 'Superman',
+      dcHeros: [ 
+        { name: 'SuperGirl' },
+        { name: 'Flash' },
+        { name: 'Batman'},
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
