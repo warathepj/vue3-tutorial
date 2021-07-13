dd7d704feefca73ca7b4cfb9a55971ba9c7403b8
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-13 20:18:41 +0700

    2 03 53

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <h1>Dc Heros {{ herosCount }}</h1>
+  <p>{{ randC }}</p>
+  <p>{{ randM() }}</p>
   <ul>
     <li v-for="(hero, index) in dcHeros" :key="index">
-      {{ index }} : {{ hero.name }}
+      {{ hero.name }}
     </li>
   </ul>
   <!--<input v-model.lazy='newHero' type="">-->
@@ -10,16 +12,28 @@
     <input v-model="newHero" placeholder="Type Hero Name Here"/>
     <button type="submit">Add Hero</button>
   </form>
+  <small>Total Heros in the list is {{ herosCount }}</small>
 </template>
 
 <script>
 export default {
+  computed: {
+    herosCount () {
+      return this.dcHeros.length + 'heros';
+    },
+    randC() {
+      return Math.random();
+    },
+  },
   methods: {
     addHero() {
       if (this.newHero !== "") {
         this.dcHeros.unshift({ name: this.newHero }); 
         this.newHero = "";
       }
+    },
+    randM() {
+      return Math.random();
     },
   },
 
@@ -31,7 +45,8 @@ export default {
         { name: "Flash" }, 
         { name: "Batman" },
         ],
-        herosCount: dcHeros.length,
+        // eslint-disable-next-line no-undef
+        //herosCount: this.dcHeros.length,
     };
   },
 };
