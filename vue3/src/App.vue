3f1e1dcd1708f7bb038982c3ef53854f719ee24a
warathepj <80021220+warathepj@users.noreply.github.com>
2021-08-04 15:42:01 +0700

    2 32 49

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,23 +1,43 @@
 <template>
-  <div id="" class="m-auto">
-    <h1 class="text-center">Dc Heros {{ herosCount }}</h1>
-    
-    <ul>
-      <li v-for="(hero, index) in dcHeros" :key="hero.name">
-        <div>{{ hero.name }} <button v-on:click="remove(index)">x</button></div>
-        <input />
-      </li>
-    </ul>
-    <form @submit.prevent="addHero">
-      <input v-model="newHero" placeholder="Type Hero Name Here"/>
-      <button type="submit">Add Hero</button>
-    </form>
+  <AppHeader />
+  <div class="w-full flex">
+    <div id="" class="m-auto">
+      <h1 class="text-2xl text-center">Dc Heros {{ herosCount }}</h1>
+      
+      <ul>
+        <li 
+          class="flex justify-between" 
+          v-for="(hero, index) in dcHeros" 
+          :key="hero.name"
+        >
+          <div>
+            {{ hero.name }} 
+          </div>
+          <button v-on:click="remove(index)">x</button>
+        </li>
+      </ul>
+      <form class="mt-10" @submit.prevent="addHero">
+        <input 
+          class="border rounded"
+          v-model="newHero" 
+          placeholder="Type Hero Name Here"
+        />
+        <button 
+          class="border rounded bg-gradient-to-r from-red-700
+          to-pink-500 text-white"
+          type="submit"
+        >
+          Add Hero
+        </button>
+      </form>
+    </div>
   </div>
-  
 </template>
 
 <script>
+import AppHeader from './components/AppHeader';
 export default {
+  components: { AppHeader },
   computed: {
     herosCount () {
       return this.dcHeros.length + 'heros';
